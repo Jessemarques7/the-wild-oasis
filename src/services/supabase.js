93eb3c2c41f41b4efdf833c373d269d6f2e43659
Jesse Marques <jessemateus919@gmail.com>
2024-05-31 21:12:03 -0300

    first commit

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://oiiexdfbxpyckfghbmbb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9paWV4ZGZieHB5Y2tmZ2hibWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcxOTMxOTMsImV4cCI6MjAzMjc2OTE5M30.qnk8u9MOSBx7Ymb9n9VPPucdLCk_HyslCfYxsxnyeGk";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
