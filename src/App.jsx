cd8ef22b4103a5b29de8ebfab879e67d04a10efa
Jesse Marques <jessemateus919@gmail.com>
2024-06-02 21:25:17 -0300

    react query

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,51 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
+
+// If you want to use React Query v5, there are only two small things to change in the project:
+
+// isLoading is now called isPending
+
+// The cacheTime option is now called gcTime
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime: 0,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{
+          margin: "8px",
+        }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -30,7 +71,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -3,8 +3,38 @@ import supabase from "./supabase";
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
   if (error) {
-    console.log("Cabins  could not be loaded");
-    throw new Error("Cabins  could not be loaded");
+    console.log("Cabins could not be loaded");
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.log("Cabin could not be deleted");
+    throw new Error("Cabin could not be deleted");
+  }
+}
+
+export async function CreateCabin(newCabin) {
+  const { name } = newCabin;
+
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([
+      {
+        ...newCabin,
+        image: `https://oiiexdfbxpyckfghbmbb.supabase.co/storage/v1/object/public/cabin-images/cabin-${name}.jpg`,
+      },
+    ])
+    .select();
+
+  if (error) {
+    console.log("Cabin could not be created");
+    throw new Error("Cabin could not be created");
   }
 
   return data;
