5887438f0df3e4c3d6663228c3d3c6d9a38b1dfa
Jesse Marques <jessemateus919@gmail.com>
2024-12-02 13:36:16 -0300

    pagination

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,30 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export default function useBookings() {
+  const [searchpararms] = useSearchParams();
+
+  const filterValue = searchpararms.get("status");
+
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
+  const sortByRaw = searchpararms.get("sortBy") || "startDate-desc";
+
+  const [field, direction] = sortByRaw.split("-");
+
+  const sortBy = { field, direction };
+
   const {
     isPending,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return {
