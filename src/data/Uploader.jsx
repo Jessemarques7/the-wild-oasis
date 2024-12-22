a3cbbc0a3c4b81ff634eae54934ab356cdeb111b
Jesse Marques <jessemateus919@gmail.com>
2024-12-22 19:57:13 -0300

    darkmode

--- src/data/Uploader.jsx
+++ src/data/Uploader.jsx
@@ -7,6 +7,7 @@ import { subtractDates } from "../utils/helpers";
 import { bookings } from "./data-bookings";
 import { cabins } from "./data-cabins";
 import { guests } from "./data-guests";
+import styled from "styled-components";
 
 // const originalSettings = {
 //   minBookingLength: 3,
@@ -125,19 +126,19 @@ function Uploader() {
     setIsLoading(false);
   }
 
+  const StyledUploader = styled.div`
+    margin-top: auto;
+    background-color: var(--color-grey-200);
+    padding: 8px;
+    border-radius: 5px;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  `;
+
   return (
-    <div
-      style={{
-        marginTop: "auto",
-        backgroundColor: "#e0e7ff",
-        padding: "8px",
-        borderRadius: "5px",
-        textAlign: "center",
-        display: "flex",
-        flexDirection: "column",
-        gap: "8px",
-      }}
-    >
+    <StyledUploader>
       <h3>SAMPLE DATA</h3>
 
       <Button onClick={uploadAll} disabled={isLoading}>
@@ -147,7 +148,7 @@ function Uploader() {
       <Button onClick={uploadBookings} disabled={isLoading}>
         Upload bookings ONLY
       </Button>
-    </div>
+    </StyledUploader>
   );
 }
 
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>todays activity</div>
+      <div>chart stay duration</div>
+      <div>sales chart</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
